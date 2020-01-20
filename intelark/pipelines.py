0416589becb4a6cd429f3643b7b49310cec12d9d
raspi <pekka.jarvinen@gmail.com>
2020-01-20 17:29:36 +0200

    Use temporary file

--- intelark/pipelines.py
+++ intelark/pipelines.py
@@ -1,24 +1,35 @@
 # -*- coding: utf-8 -*-
 import os
 import json
+from tempfile import NamedTemporaryFile
 from pathlib import Path
+from shutil import move
 from intelark.items import *
 
 
 class IntelarkPipeline(object):
+    """
+    Save CPU specifications
+    """
+
     def process_item(self, item, spider):
         if isinstance(item, CPUSpecsItem):
-            fpath = os.path.abspath(os.path.join("..", "items", "cpuspecs", item["socket"]))
+            # Save to temporary file
+            tmpf = NamedTemporaryFile("w", prefix="cpu-specs-", suffix=".json", encoding="utf8", delete=False)
+            with tmpf as f:
+                json.dump(item, f)
+                f.flush()
+                spider.logger.info(f"saved as {f.name}")
 
+            # path for saving
+            fpath = os.path.abspath(os.path.join("..", "items", "cpuspecs", item["socket"]))
             Path(fpath).mkdir(parents=True, exist_ok=True)
             fname = item["id"] + ".json"
-
             fullpath = os.path.join(fpath, fname)
 
-            with open(fullpath, "w", encoding="utf8") as f:
-                json.dump(item, f)
-                spider.logger.info(f"saved {f.name}")
-
+            # Rename and move the temporary file to actual file
+            newpath = move(tmpf.name, fullpath)
+            spider.logger.info(f"renamed {tmpf.name} to {newpath}")
             return
 
         spider.logger.error("Skipped item {0}".format(type(item)))
